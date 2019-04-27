4d0a297e9ee66dda74a848baaa5dc1c2f1a70827
Brian Illes <brianrilles@gmail.com>
2019-04-26 19:28:41 -0700

    Wires up messenge form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Message from "./Message.js";
 
-function App() {
-  return (
-    <div className="App">
-      <p>sanity</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>
+          Websockets Demo 4/29/19. Repo{" "}
+          <a href="https://github.com/brilles/websockets-demo">here</a>{" "}
+        </h2>
+        <Message />
+      </div>
+    );
+  }
 }
 
 export default App;
